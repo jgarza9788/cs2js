23af68589e5ea86d6fc7533b030be4602abbd1e6
jgarza9788 <jgarza9788@gmail.com>
2023-03-14 20:22:53 -0400

    Initial commit

---
+++ cs2js/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs2js
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
